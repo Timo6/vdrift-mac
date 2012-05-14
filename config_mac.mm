30e1634f81eb1c2edfceb1b7baa99bc31dd1cded
Timothy Furlong <timothyfurlong6@gmail.com>
2012-05-14 14:45:24 +0100

    If data not found inside or outside app, ask user to tell us where it is
    Currently asks twice, not sure why.

--- config_mac.mm
+++ config_mac.mm
@@ -12,9 +12,61 @@ char* get_mac_data_dir()
     {
         path = [resourcePath stringByAppendingString:@"/data"];
     }
-    else
+    else if ([[NSFileManager defaultManager] fileExistsAtPath:[basePath stringByAppendingString:@"/data/settings/options.config"]])
     {
         path = [basePath stringByAppendingString:@"/data"];
+    }
+    else
+    {
+        NSApplication   *myApplication;
+        myApplication = [NSApplication sharedApplication];
+        
+        NSAlert *firstAlert = [NSAlert alertWithMessageText: @"Can't find data"
+                                              defaultButton: @"Choose"
+                                            alternateButton: @"Quit"
+                                                otherButton: nil
+                                  informativeTextWithFormat: @"Please choose the data folder."];
+        
+        
+        if ([firstAlert runModal] == NSAlertDefaultReturn)
+        {
+            NSOpenPanel *findData = [NSOpenPanel openPanel];
+            [findData setAllowsMultipleSelection:NO];
+            [findData setCanChooseDirectories:YES];
+            [findData setCanChooseFiles:NO];
+            [findData setCanCreateDirectories:NO];
+            [findData setResolvesAliases:NO];
+            [findData setDirectory:basePath];
+            [findData setTitle:@"Choose Data Folder"];
+            
+            if ([findData runModal] == NSFileHandlingPanelOKButton)
+            {
+                path = [[findData filenames] objectAtIndex:0];
+                
+                if (![[NSFileManager defaultManager] fileExistsAtPath:[path stringByAppendingString:@"/settings/options.config"]])
+                {
+                    NSAlert *secondAlert = [NSAlert alertWithMessageText: @"Can't find data"
+                                                           defaultButton: @"Quit"
+                                                         alternateButton: nil
+                                                             otherButton: nil
+                                               informativeTextWithFormat: @"Please check the folder you choose contains the data."];
+                    [secondAlert runModal];
+                    
+                    [pool release];
+                    exit(1);
+                }
+            } 
+            else
+            {
+                [pool release];
+                exit(1);
+            }
+        }
+        else 
+        {
+            [pool release];
+            exit(1);
+        }
     }
     
     CFStringRef resolvedPath = nil;
